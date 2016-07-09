f53df5ff6597f04a27ced5510a4af19107d50258
Jeff Rames <jeff@floodlightsoftware.com>
2016-07-09 17:58:49 -0500

    final project added

---
+++ user-notifications/projects/final/ContentExtension/NotificationViewController.swift
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+      guard let attachment = notification.request.content.attachments.first
+        else { return }
+      
+      if attachment.url.startAccessingSecurityScopedResource() {
+        imageView.image = UIImage(contentsOfFile: attachment.url.path!)
+        attachment.url.stopAccessingSecurityScopedResource()
+      }
+    }
+  func didReceive(_ response: UNNotificationResponse, completionHandler
+    completion: (UNNotificationContentExtensionResponseOption) -> Void) {
+    if response.actionIdentifier == "star" {
+      imageView.showStars()
+      let time = DispatchTime.now() + DispatchTimeInterval.milliseconds(2000)
+      DispatchQueue.main.after(when: time) {
+        completion(.dismissAndForwardAction)
+      }
+    } else if response.actionIdentifier == "dismiss" {
+      completion(.dismissAndForwardAction)
+    }
+  }
+}
---
+++ user-notifications/projects/final/CuddlePix/AppDelegate.swift
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import UserNotifications
+
+let userNotificationReceivedNotificationName = Notification.Name("com.raywenderlich.CuddlePix.userNotificationReceived")
+let newCuddlePixCategoryName = "newCuddlePix"
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    UNUserNotificationCenter.current().delegate = self
+    configureUserNotifications()
+    application.registerForRemoteNotifications()
+    return true
+  }
+  
+  private func configureUserNotifications() {
+    let starAction = UNNotificationAction(identifier: "star", title: "🌟 star my cuddle 🌟 ", options: [])
+    let dismissAction = UNNotificationAction(identifier: "dismiss", title: "Dismiss", options: [])
+    
+    let category = UNNotificationCategory(identifier: newCuddlePixCategoryName,
+                                          actions: [starAction, dismissAction],
+                                          minimalActions: [starAction, dismissAction],
+                                          intentIdentifiers: [],
+                                          options: [])
+    
+    UNUserNotificationCenter.current().setNotificationCategories([category])
+  }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+                              withCompletionHandler completionHandler:
+    (UNNotificationPresentationOptions) -> Void) {
+    NotificationCenter.default.post(name:
+      userNotificationReceivedNotificationName, object: .none)
+    completionHandler(.alert)
+  }
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: () -> Void) {
+    print("Response received for \(response.actionIdentifier)")
+    completionHandler()
+  }
+}
+
+extension AppDelegate {
+  func application(_ application: UIApplication,
+                   didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+    print("Registration for remote notifications failed")
+    print(error.localizedDescription)
+  }
+  func application(_ application: UIApplication,
+                   didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    print("Registered with device token: \(deviceToken.hexString)")
+  }
+}
